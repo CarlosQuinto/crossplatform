bba2c67cf5df81774faf4cf9bbefe0226978e357
JhonLP <jhonlpdev@gmail.com>
2015-10-07 20:08:45 -0700

    Archivos base

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        console.log("Service initialized");
+    });
+
+    /* --------------------------------- Event Registration -------------------------------- */
+    $('.search-key').on('keyup', findByName);
+    $('.help-btn').on('click', function() {
+        alert("App de contactos 1.0");
+    });
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+    function findByName() {
+        service.findByName($('.search-key').val()).done(function (employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i = 0; i < l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    }
+
+}());
