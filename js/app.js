534be1c576d00c5698395a7240b4bf0d4507d5c6
JhonLP <jhonlpdev@gmail.com>
2015-10-09 14:19:52 -0700

    Agregada vista de empleado e implementada su ruta

--- js/app.js
+++ js/app.js
@@ -3,10 +3,19 @@
     //Ya no son variables locales, son objetos de las vistas
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html()); // Guarda el template home
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html()); // Guarda el template empleados
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     /* ---------------------------------- Variables Locales ---------------------------------- */
     var service = new EmployeeService();
     service.initialize().done(function () {
         $('body').html(new HomeView(service).render().$el);
+
+        router.addRoute('employees/:id', function(id) { //creamos una nueva ruta que acepta un argumento (el id del empleado)
+            service.findById(parseInt(id)).done(function(employee) { //convertimos el argumento a un entero
+                $('body').html(new EmployeeView(employee).render().$el); //renderisamos la vista rellenando los datos con el empleado
+            });
+        });
+
+        router.start();
     });
 
     /* --------------------------------- Registro de Eventos -------------------------------- */
