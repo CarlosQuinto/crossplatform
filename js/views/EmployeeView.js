e9d9e631e52f749b3b98c458844e86add90331f1
JhonLP <jhonlpdev@gmail.com>
2015-10-10 17:37:53 -0700

    Agregada funcion de geolocalizacion

--- js/views/EmployeeView.js
+++ js/views/EmployeeView.js
@@ -2,11 +2,24 @@ var EmployeeView = function(employee) {
 
   this.initialize = function() {
       this.$el = $('<div/>');
+      this.$el.on('click', '.add-location-btn', this.addLocation); //registramos el listener de geolocalizacion
   };
 
   this.render = function() {
       this.$el.html(this.template(employee));
       return this;
+  };
+
+    this.addLocation = function(event) {
+    event.preventDefault(); //evitamos el comportamiento por default de la geolocalizacion
+    navigator.geolocation.getCurrentPosition( //obtenemos la posicion actual
+        function(position) {
+            alert(position.coords.latitude + ',' + position.coords.longitude); //mostramos las cordenadas
+        },
+        function() {
+            alert('Error obteniendo la geolocalizacion');
+        });
+    return false;
   };
 
   this.initialize();
